45e87d2829fa2dbd2e57ab8e724ff7f45c00585d
tkuester <tobias.kuester@gmx.net>
2015-06-11 22:43:24 +0200

    added activity information to canvas, added docs

--- ActivityLogger/activity_model.py
+++ ActivityLogger/activity_model.py
@@ -1,9 +1,10 @@
-
 import json
 import time
 
 
 class Interval:
+	""" 'Enumeration' of different time intervals
+	"""
 	DAY = "DAY"
 	WEEK = "WEEK"
 	MONTH = "MONTH"
@@ -11,6 +12,10 @@ class Interval:
 
 
 class Activity:
+	""" Class describing an activity. This includes some information about the
+	activity as well as how many instances of this activity to perform in what
+	time interval, and when this activity has actually been carried out.
+	"""
 	
 	def __init__(self, name, description, interval, required=1, history=None):
 		self.name = name
@@ -24,33 +29,44 @@ class Activity:
 				% (self.name, self.description, self.interval, self.required, self.history)
 	
 	def log_now(self):
+		""" Add the current date and time to the activity's history.
+		"""
 		t = time.time()
 		print "logged", time.ctime(t)
 		self.history.append(t)
 	
 	def get_counts(self):
-		pass
+		""" Calculate how often this activity has been performed in each time
+		interval based on the timestamps from the history.
+		"""
+		import random
+		return [random.randint(0, self.required * 2) for i in range(random.randint(10, 30))]
 		# TODO timestamps in dates umwandeln; mit groupby nach interval
 		# gruppieren, laenge bestimmen und in liste eintragen 
 		# --> vorsicht mit leeren intervallen! nicht einfach vergessen
 
 
 def load_activities(filename):
+	""" Load activities from JSON file.
+	"""
 	with open(filename) as f:
 		lst = json.load(f)
 		return [Activity(**d) for d in lst]
 
 
 def store_activities(activities, filename):
+	""" Stora activities in JSON file.
+	"""
 	with open(filename, "w") as f:
 		lst = [act.__dict__ for act in activities]
 		json.dump(lst, f)
 	
 
+
 if __name__ == "__main__":
 	
-	a1 = Activity("a1", "test activity 1", Interval.WEEK, 2)
-	a2 = Activity("a2", "test activity 2", Interval.DAY, 1)
+	a1 = Activity("Some activity 1", "Description for activity 1", Interval.WEEK, 2)
+	a2 = Activity("Some other activity 2", "Some bla bla about activity 2", Interval.DAY, 1)
 	print a1
 	print a2
 	
--- ActivityLogger/activity_ui.py
+++ ActivityLogger/activity_ui.py
@@ -1,7 +1,11 @@
-from Tkinter import *
 from activity_model import *
+from Tkinter import *
+import tkFont
+
 
 class ActivityFrame(Frame):
+	""" Simple Tkinter Frame for displaying and interacting with activity list.
+	"""
 	
 	def __init__(self, activities, master=None):
 		Frame.__init__(self, master)
@@ -10,10 +14,13 @@ class ActivityFrame(Frame):
 			panel = ActivityPanel(self, act)
 			panel.pack(side=TOP)
 		self.pack()
+		self.bind_all("<KeyPress-q>", lambda e: self.quit())
 
 
 class ActivityPanel(Canvas):
-	
+	""" Panel for showing, editing and logging an individual activity.
+	"""
+
 	def __init__(self, parent, activity):
 		Canvas.__init__(self, parent)
 		self.activity = activity
@@ -21,11 +28,36 @@ class ActivityPanel(Canvas):
 		self.canvas = Canvas(self, width=200, height=50)
 		self.canvas.pack(side=LEFT)
 		
+		#TODO button 'stats'
+		#TODO button 'edit'
+		
 		self.log_button = Button(self, text="LOG", relief=FLAT, command=self.log_now)
 		self.log_button.pack(side=RIGHT)
 		
+		self.font_name = tkFont.Font(family="Helvetica", size=12)
+		self.font_desc = tkFont.Font(family="Helvetica", size=8)
+		
+		self.update_visuals()
+		
+	def update_visuals(self):
+		""" Update the information displayed on the canvas.
+		"""
+		self.activity.name = "aaaaaaaaaaaa"
+		self.canvas.delete("all")
+		self.canvas.create_text(2, 2, text=self.activity.name, font=self.font_name, anchor=NW)
+		self.canvas.create_text(2, 20, text=self.activity.description, font=self.font_desc, anchor=NW)
+		counts = self.activity.get_counts()
+		for i, c in enumerate(reversed(counts[-20:]), 1):
+			x = 200 - i * 10
+			color = "#00ff00" if c > self.activity.required else "#ff0000"
+			self.canvas.create_rectangle(x, 40, x + 6, 46, outline=color, fill=color)
+		
 	def log_now(self):
+		""" Log this activity as having been performed right now.
+		"""
 		self.activity.log_now()
+		self.update_visuals()
+
 
 
 if __name__ == "__main__":
