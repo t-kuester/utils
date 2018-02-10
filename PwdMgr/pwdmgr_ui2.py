0973189f4cd559ce080f174b9cfe36efa5db581b
t-kuester <tobias.kuester@gmx.net>
2018-02-10 18:51:36 +0100

    password filtering

--- PwdMgr/pwdmgr_ui2.py
+++ PwdMgr/pwdmgr_ui2.py
@@ -16,20 +16,20 @@ import pwdmgr_core, pwdmgr_model, config
 import os
 
 
+ATTRIBUTES = ("label", "username", "password", "url", "tags", "last_changed")
+
 class PwdMgrFrame(tkinter.Frame):
 	
 	def __init__(self, root, conf, filename):
 		super().__init__(root)
 		self.conf = conf
 		self.filename = filename
-		
 		self.grid()
 
-		fltr = tkinter.StringVar()
-		fltr.trace("w", self.filter_list)
+		self.fltr = tkinter.StringVar()
+		self.fltr.trace("w", self.filter_list)
 		
-		tkinter.Entry(self, textvariable=fltr).grid(row=0, column=0, sticky="W")
-
+		tkinter.Entry(self, textvariable=self.fltr).grid(row=0, column=0, sticky="EW")
 		tkinter.Button(self, text="<<", command=self.clear_fltr).grid(row=0, column=1, sticky="EW")	
 		tkinter.Button(self, text="+", command=self.add_password).grid(row=0, column=2, sticky="EW")
 		tkinter.Button(self, text="-", command=self.remove_password).grid(row=0, column=3, sticky="EW")
@@ -37,58 +37,59 @@ class PwdMgrFrame(tkinter.Frame):
 
 		self.table = PwdTable(self)
 		self.table.grid(row=1, column=0, columnspan=5)
-		
+		self.table.show_passwords(self.conf.passwords)
+		# TODO wrap table in scrollpane
+
 		# TODO
-		# buttons: add, remove, save list
 		# buttons: copy, open, changed now, generate
-		# search/filter
 		
 	def clear_fltr(self):
-		pass
+		print("clearing fitler")
+		self.fltr.set("")
 		
 	def filter_list(self, *args):
-		pass
+		print("filtering...")
+		s = self.fltr.get()
+		filtered = [p for p in self.conf.passwords
+		            if any(s in getattr(p, a) for a in ATTRIBUTES)]
+		self.table.show_passwords(filtered)
 		
 	def save(self):
+		print("saving...")
 		pwdmgr_core.save_encrypt(self.filename, self.conf)
 
 	def add_password(self):
+		print("adding password")
 		p = pwdmgr_model.Password("", "", "", "", "", "")
 		self.conf.passwords.append(p)
+		self.filter_list()
 		
 	def remove_password(self):
-		p = self.get_selected()
-		if p:
-			self.config.passwords.remove(p)
-			self.update_options()
-			self.selected.set(next((p.label for p in self.conf.passwords), None))
+		print("removing password")
+		# TODO how to determine which one? show dialogue? get row of widget with focus?
+		self.filter_list()
 
 
 class PwdTable(tkinter.Canvas):
 	
 	def __init__(self, parent):
 		super().__init__(parent)
-		self.parent = parent
-		attributes = ("label", "username", "password", "url", "tags", "last_changed")
-		
 		self.grid()
 		
-		# widgets = []
-		
-		for col, attribute in enumerate(attributes):
+	def show_passwords(self, passwords):
+		# remove old children from grid
+		for child in self.grid_slaves():
+			child.grid_forget()
+		# add row with the attribute names
+		for col, attribute in enumerate(ATTRIBUTES):
 			tkinter.Label(self, text=attribute).grid(row=0, column=col, sticky="NW")
-		
-		for row, pwd in enumerate(self.parent.conf.passwords, start=1):
-			# d = {}
-			for col, attribute in enumerate(attributes):
+		# add rows with filtered passwords
+		for row, pwd in enumerate(passwords, start=1):
+			for col, attribute in enumerate(ATTRIBUTES):
 				var = tkinter.StringVar()
 				var.set(getattr(pwd, attribute))
 				tkinter.Entry(self, textvariable=var).grid(row=row, column=col, sticky="W")
 				var.trace("w", lambda *args, v=var, a=attribute, p=pwd: setattr(p, a, v.get()))
-				
-				# d[attribute] = var
-			# widgets.append(d)
-	
 
 
 if __name__ == "__main__":
